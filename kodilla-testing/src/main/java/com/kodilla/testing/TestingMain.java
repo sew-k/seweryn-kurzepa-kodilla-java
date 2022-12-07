7a181f874c0aed2931ea03043b88f82cb732907f
Seweryn Kurzepa <sew_k@wp.pl>
2022-12-07 17:40:07 +0100

    6.2 First unit test for Calculator class - attempt 2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,44 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testów oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // First Unit test
+
+        System.out.println("Beginning first unit test fo Calculator class:");
+        Calculator calculator = new Calculator();
+
+        int testAValue = 14;
+        int testBValue = 2;
+
+        int addResult = calculator.add(testAValue, testBValue);
+
+        if (addResult == (testAValue + testBValue)) {
+            System.out.println("Result of addiction values " + testAValue + " and " + testBValue + " - OK");
+        } else {
+            System.out.println("Error in calculated addition");
+        }
+
+        int subtractResult = calculator.subtract(testAValue, testBValue);
+
+        if (subtractResult == (testAValue - testBValue)) {
+            System.out.println("Result of subtraction values " + testAValue + " and " + testBValue + " - OK");
+        } else {
+            System.out.println("Error in calculated subtraction");
+        }
+
     }
+
 }
