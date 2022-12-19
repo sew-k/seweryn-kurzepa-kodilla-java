04cb58766dab23823e9c4f5a7ca0b2805611b244
Seweryn Kurzepa <sew_k@wp.pl>
2022-12-19 13:03:33 +0100

    Added 7.1 method 'beautifier' to main method

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.forumuser.Forum;
@@ -13,6 +14,30 @@ import java.util.stream.Collectors;
 public class StreamMain {
 
     public static void main(String[] args) {
+
+        //7.1 module task
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("An example of text", text -> "-|- " + text + " -|-");
+        poemBeautifier.beautify("An example of text", text -> {
+            String str = new String(text);
+            text = str.toUpperCase();
+            return text;
+        });
+        poemBeautifier.beautify("An example of text", text ->  {
+            String str = new String(text);
+            text = str.toLowerCase();
+            return text;
+        });
+        poemBeautifier.beautify("An example of text", text ->  {
+            String str = new String(text);
+            char[] textArray = str.toCharArray();
+            String spaces = "";
+            for (int i=0; i<textArray.length; i++) {
+                System.out.println(spaces + textArray[i]);
+                spaces += "   ";
+            };
+            return "voila!";
+        });
 
         BookDirectory theBookDirectory = new BookDirectory();
         String theResultStringOfBooks = theBookDirectory.getList().stream()
