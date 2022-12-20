d00afa569376c3c25257f67b9061a7cfcba34af4
Seweryn Kurzepa <sew_k@wp.pl>
2022-12-20 23:55:03 +0100

    7.5 - IntStream for average value calculations and ArrayOperationsTestSuite

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,28 @@
+package com.kodilla.stream.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
+public interface ArrayOperations {
+
+    static double getAverage(int[] numbers) {
+
+        Integer[] numbersInt = new Integer[numbers.length];
+        for (int i=0; i<numbers.length; i++) {
+            numbersInt[i] = (Integer) numbers[i];
+        }
+        List<Integer> numbersList = new ArrayList<>(Arrays.asList(numbersInt));
+
+        IntStream.range(0, numbersList.size())
+                .map(n -> numbersList.get(n))
+                .forEach(System.out::println);
+
+        OptionalDouble result = IntStream.range(0, numbersList.size())
+                .average();
+
+        return result.getAsDouble();
+    }
+}
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayOperationTestSuite {
+
+    @Test
+    void testGetAverage() {
+        //Given
+        int[] numbers = new int[10];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 5;
+        numbers[5] = 6;
+        numbers[6] = 7;
+        numbers[7] = 8;
+        numbers[8] = 9;
+        numbers[9] = 10;
+
+        //When
+        double average = ArrayOperations.getAverage(numbers);
+        double expectedAverage = (double)(1+2+3+4+5+6+7+8+9) / 10;
+
+        //Then
+        assertEquals(expectedAverage, average);
+    }
+}
