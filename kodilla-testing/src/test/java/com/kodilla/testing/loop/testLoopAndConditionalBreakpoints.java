3187436d217819c5c6a2b57a0e5b42a233ba158b
Seweryn Kurzepa <sew_k@wp.pl>
2023-02-21 19:38:50 +0100

    15.6 - Task with getValue debugging: printscreen image upload

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.loop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class testLoopAndConditionalBreakpoints {
+    @Test
+    void testLoop() {
+        //Given
+        long sum = 0;
+        //When
+        for(int n = 0; n < 1000; n++) {
+            sum += n;
+            System.out.println("[" + n + "]" + " sum equals: " + sum);
+        }
+        //Then
+        Assertions.assertEquals(499500, sum);
+    }
+}
