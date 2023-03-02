e054c1d9e5966cb06fac903a783284c40b625aaa
Seweryn <sew_k@wp.pl>
2023-03-02 20:36:29 +0100

    17.2.c - datasource and mySQL

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -1,7 +1,5 @@
 package com.kodilla.hibernate.task;
 
-
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/Application.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/Application.java
@@ -2,6 +2,7 @@ package com.kodilla.jdbc;
 
 public class Application {
     public static void main(String[] args) {
-
+        DBInitializer dbInitializer = new DBInitializer();
+        dbInitializer.initializeDatabase();
     }
 }
